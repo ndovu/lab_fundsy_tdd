a25783ff9d50af78220f6ee4e6fa441245bd42b5
Noel Bacani <mail@ndovutech.com>
2015-05-07 19:50:45 -0700

    created four tests for the user model concerning the validations for the user

--- db/migrate/20150508005449_create_users.rb
+++ db/migrate/20150508005449_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :fist_name
+      t.string :first_name
       t.string :last_name
       t.string :email
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  def valid_attributes(new_attributes = {}) 
+    attributes = {first_name: "Noel",
+                  last_name: "Bacani",
+                  email: "noel.bacani@gmail.com"}
+    attributes.merge(new_attributes)
+  end
+
+  describe "Validations" do
+    it 'requires an email' do
+      user = User.new(valid_attributes({email: nil}))
+      expect(user).to be_invalid
+    end
+
+    it 'requires a first name' do
+      user = User.new(valid_attributes({first_name: nil}))
+      expect(user).to be_invalid
+    end
+
+    it 'requires a unique email' do
+      # Create a user with email in memory
+      User.create(valid_attributes)
+      # Try to create another user with the same email. Should throw an error
+      user = User.new(valid_attributes)
+      user.save
+      expect(user.errors.messages).to have_key(:email)
+    end
+
+    it 'requires a valid email' do
+      user = User.new(valid_attributes({email: 'not_a_valid_email'}))
+      expect(user).to be_invalid
+
+    end
+  end
+
 end
